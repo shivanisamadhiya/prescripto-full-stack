35e4536764ff9c199711284dc1479f80b45bfb22
shivani samadhiya <shivanisamadhiya229489@gmail.com>
2026-04-29 20:14:17 +0530

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto is dedicated to making healthcare simpler and more accessible for everyone. Our platform helps you understand your health concerns and connects you instantly with a trusted network of experienced specialists, from Pediatricians to Neurologists. Whether you're looking for a quick consultation or a long-term health partner, we are here to ensure that booking your next doctor’s appointment is just a click away. Your health is our priority, and we strive to bring expert medical care right to your fingertips.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91-9000000000</li>
+            <li>shivani123@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2026 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
